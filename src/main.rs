e21ac7b8590d83f4f54479d1168a15d6627dd966
Martial <martial@marels.fr>
2024-06-22 18:06:02 +0200

    feat: add print version

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,7 @@ fn main() {
     println!("Hello, World !");
     println!("Bonjour, Monde !");
     println!("Hola, Mundo !");
+
+    let version = env!("CARGO_PKG_VERSION");
+    println!("\r\nVersion: {}", version);
 }
